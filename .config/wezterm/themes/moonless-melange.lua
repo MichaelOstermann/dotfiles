de85f7fd5af82f61d951e01a2558e7263bf2d6b1
Michael Ostermann <michaelostermann@me.com>
2024-10-19 14:22:05 +0200

    feat: add wezterm

---
+++ .config/wezterm/themes/moonless-melange.lua
@@ -0,0 +1,52 @@
+return function(config)
+    config.window_frame = {
+        active_titlebar_bg = "#231f1d",
+        inactive_titlebar_bg = "#231f1d",
+    }
+
+    config.colors = {
+        foreground = "#817b75",
+        background = "#231f1d",
+        cursor_bg = "#817b75",
+        cursor_border = "#817b75",
+        cursor_fg = "#231f1d",
+        selection_bg = "#383431",
+        selection_fg = "none",
+        split = "#1c1815",
+        tab_bar = {
+            inactive_tab_edge = "none",
+            active_tab = {
+                bg_color = "#231f1d",
+                fg_color = "#8a8fad",
+            },
+            inactive_tab = {
+                bg_color = "#231f1d",
+                fg_color = "#7a736e",
+            },
+            inactive_tab_hover = {
+                bg_color = "#2f2b28",
+                fg_color = "#8f8882",
+            },
+        },
+        ansi = {
+            "#544f4b",
+            "#D47766",
+            "#62836c",
+            "#987d4a",
+            "#8a8fad",
+            "#95718b",
+            "#5c7576",
+            "#68635e",
+        },
+        brights = {
+            "#544f4b",
+            "#D47766",
+            "#62836c",
+            "#987d4a",
+            "#8a8fad",
+            "{terminal.magenta}",
+            "#5c7576",
+            "#68635e",
+        },
+    }
+end
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("IosevkaTerm Nerd Font")
+config.font_size = 19
+config.line_height = 1.06
+config.harfbuzz_features = { "calt=0" }
+config.send_composed_key_when_left_alt_is_pressed = true
+
+config.window_decorations = "RESIZE"
+config.adjust_window_size_when_changing_font_size = false
+config.hide_tab_bar_if_only_one_tab = true
+config.scrollback_lines = 5000
+config.show_new_tab_button_in_tab_bar = false
+
+config.keys = {
+    {
+        mods = "CMD",
+        key = "d",
+        action = wezterm.action.SplitPane({
+            direction = "Right",
+            size = { Percent = 35 },
+        }),
+    },
+    {
+        mods = "CMD",
+        key = "w",
+        action = wezterm.action.CloseCurrentPane({ confirm = true }),
+    },
+    {
+        mods = "CMD",
+        key = "LeftArrow",
+        action = wezterm.action.ActivateTabRelative(-1),
+    },
+    {
+        mods = "CMD",
+        key = "RightArrow",
+        action = wezterm.action.ActivateTabRelative(1),
+    },
+}
+
+require("themes/moonless-tokyonight")(config)
+
+return config
