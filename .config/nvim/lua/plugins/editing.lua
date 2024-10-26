e0c560b70aac9a75464a0150f60ecbae5e2d4564
Michael Ostermann <michaelostermann@me.com>
2024-10-26 15:03:07 +0200

    fix(nvim): disable wildfire <bs> mapping

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -92,6 +92,11 @@ return {
         event = "VeryLazy",
         opts = {
             filetype_exclude = require("utils.filetypes").special_filetypes,
+            keymaps = {
+                init_selection = "<CR>",
+                node_incremental = "<CR>",
+                node_decremental = "<NOP>",
+            },
         },
     },
 
