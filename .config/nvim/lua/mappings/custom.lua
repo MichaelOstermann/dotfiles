2e037de85e0ba63e770c8a8c41efd0665fe3d069
Michael Ostermann <michaelostermann@me.com>
2025-06-26 23:06:48 +0200

    fix(nvim): temp disable async/await handler

--- .config/nvim/lua/mappings/custom.lua
+++ .config/nvim/lua/mappings/custom.lua
@@ -204,32 +204,32 @@ au("FileType", function()
 end)
 
 -- https://github.com/JoosepAlviste/dotfiles/blob/master/config/nvim/lua/j/javascript.lua
-map("i", "t", function()
-    vim.api.nvim_paste("t", false, -1)
-
-    if should_skip() then
-        return
-    end
-
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local line = vim.api.nvim_get_current_line()
-    local text_before = line:sub(col - 4, col)
-
-    if text_before ~= "await" then
-        return
-    end
-
-    local function_node =
-        b.find_node_ancestor({ "arrow_function", "function_declaration", "function" })
-    if not function_node then
-        return
-    end
-
-    local function_text = vim.treesitter.get_node_text(function_node, 0)
-    if vim.startswith(function_text, "async ") then
-        return
-    end
-
-    local start_row, start_col = function_node:start()
-    vim.api.nvim_buf_set_text(0, start_row, start_col, start_row, start_col, { "async " })
-end)
+-- map("i", "t", function()
+--     vim.api.nvim_paste("t", false, -1)
+--
+--     if should_skip() then
+--         return
+--     end
+--
+--     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+--     local line = vim.api.nvim_get_current_line()
+--     local text_before = line:sub(col - 4, col)
+--
+--     if text_before ~= "await" then
+--         return
+--     end
+--
+--     local function_node =
+--         b.find_node_ancestor({ "arrow_function", "function_declaration", "function" })
+--     if not function_node then
+--         return
+--     end
+--
+--     local function_text = vim.treesitter.get_node_text(function_node, 0)
+--     if vim.startswith(function_text, "async ") then
+--         return
+--     end
+--
+--     local start_row, start_col = function_node:start()
+--     vim.api.nvim_buf_set_text(0, start_row, start_col, start_row, start_col, { "async " })
+-- end)
